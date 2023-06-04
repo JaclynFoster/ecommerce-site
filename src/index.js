d9c961533b678b731218daf2a4990142a42a0a2f
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-06-04 17:38:31 -0500

    setup basic project shell and react router

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Home from './components/Home/Home';
+import Animals from './components/Animals/Animals';
+import Cart from './components/Cart/Cart';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
+import reportWebVitals from './reportWebVitals';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  }, 
+  {
+    path: "/animals",
+    element: <Animals/>,
+  },
+  {
+   path: "/cart",
+   element: <Cart />,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+    {/* <App /> */}
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
